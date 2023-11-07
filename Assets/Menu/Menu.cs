d9b1d755431f09684f3e88b4daf897ffaf1c4c24
aurelianocaetano <150064591+aurelianocaetano@users.noreply.github.com>
2023-11-07 17:00:27 -0300

    Instruçoes

--- Assets/Menu/Menu.cs
+++ Assets/Menu/Menu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Menu : MonoBehaviour
 {
@@ -12,10 +14,9 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("teste");
         botaoJogar.onClick.AddListener(() =>
         {
-            Debug.Log("Clicou no botão de jogar");
+            SceneManager.LoadScene("SampleScene");
         });
     }
 }
